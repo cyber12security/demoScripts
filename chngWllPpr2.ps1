d04177872ae478dea879f72730fde68142e7e5ff
cyber12security <95104236+cyber12security@users.noreply.github.com>
2021-12-31 14:01:03 -0600

    Modified timer

--- chngWllPpr2.ps1
+++ chngWllPpr2.ps1
@@ -2,8 +2,7 @@ $location = Get-Location;
 $grabUser = $env:UserName
 reg add "HKCU\Control Panel\Desktop" /v WallPaper /d "C:\Users\$grabUser\youHaveBeenHacked.jpg" /f;
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers\" /v CurrentWallpaperPath /d "C:\Users\$grabUser\youHaveBeenHacked.jpg" /f;
-RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True;
-Start-Sleep -Seconds 10;
+Start-Sleep -Seconds 35;
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True;
 Start-Sleep -Seconds 1;
 Exit;
