a90864a7c6761e68bd3e1f3ea5c95850c6b98753
cyber12security <95104236+cyber12security@users.noreply.github.com>
2021-12-31 13:45:43 -0600

    Fixed variable

--- chngWllPpr2.ps1
+++ chngWllPpr2.ps1
@@ -1,5 +1,5 @@
 $location = Get-Location;
-$grabUser = env:UserName
+$grabUser = $env:UserName
 reg add "HKCU\Control Panel\Desktop" /v WallPaper /d "C:\Users\$grabUser\youHaveBeenHacked.jpg" /f;
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers\" /v CurrentWallpaperPath /d "C:\Users\$grabUser\youHaveBeenHacked.jpg" /f;
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True;
