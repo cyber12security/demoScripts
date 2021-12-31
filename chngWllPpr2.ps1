17e2ac7db61330b9afb91d806f264b7e3e98be32
cyber12security <95104236+cyber12security@users.noreply.github.com>
2021-12-31 13:18:58 -0600

    Add files via upload

---
+++ chngWllPpr2.ps1
@@ -0,0 +1,8 @@
+$location = Get-Location;
+reg add "HKCU\Control Panel\Desktop" /v WallPaper /d "$location\youHaveBeenHacked.jpg" /f;
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers\" /v CurrentWallpaperPath /d "$location\youHaveBeenHacked.jpg" /f;
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True;
+Start-Sleep -Seconds 10;
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True;
+Start-Sleep -Seconds 1;
+Exit;
